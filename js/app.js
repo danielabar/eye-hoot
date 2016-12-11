052b39e2ef03cdc82885706bedabb71356b81896
Daniela Baron <baron.daniela@gmail.com>
2016-12-10 23:18:41 -0500

    move eyeshine side to side on a smaller scale for more realistic effect

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
   var start = function() {
     document.querySelector('.side-to-side').addEventListener('click', function(evt) {
       document.querySelector('.eyes').classList.toggle('side-to-side');
+      document.querySelector('.left-eye-shine').classList.toggle('side-to-side');
+      document.querySelector('.right-eye-shine').classList.toggle('side-to-side');
     });
     document.querySelector('.up-down').addEventListener('click', function(evt) {
       document.querySelector('.eyes').classList.toggle('up-down');
