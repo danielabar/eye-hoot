fb1cd5f1fd56b53688453aa246099117116616dc
Daniela Baron <baron.daniela@gmail.com>
2016-12-31 15:36:06 -0500

    [15] increase eye exercise duration to 15 seconds

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -14,7 +14,7 @@ var cleanPlugin = new CleanWebpackPlugin(['docs'], {
 
 // environment specific config
 var definePlugin = new webpack.DefinePlugin({
-  DEFAULT_EYE_EXERCISE_DURATION: "10",    // 10 seconds
+  DEFAULT_EYE_EXERCISE_DURATION: "15",    // 15 seconds
   DEFAULT_LONG_BREAK_DURATION: "300",     // 5 * 60 = 5 minutes
   DEFAULT_EYE_EXERCISE_INTERVAL: "900",   // 15 * 60 = 15 minutes
   DEFAULT_LONG_BREAK_INTERVAL: "3600"     // 60 * 60 = 1 hour
