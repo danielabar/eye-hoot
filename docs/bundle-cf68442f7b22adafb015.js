0547dbd25caa34de8b2b2f3f3edd381272dd14d7
Daniela Baron <baron.daniela@gmail.com>
2017-01-15 16:14:11 -0500

    [#26] adjust hover colors

--- docs/bundle-cf68442f7b22adafb015.js
+++ docs/bundle-cf68442f7b22adafb015.js
@@ -44,4 +44,4 @@ function(e,t,n){e.exports=n.p+"b506a66a688dd0df4b191f1ff1611c45.mp3"},/*!*******
   !*** ./sounds/tweet.mp3 ***!
   \**************************/
 function(e,t,n){e.exports=n.p+"9a62c3d1f3ca4b470bc4a446f5c9c9e1.mp3"}]);
-//# sourceMappingURL=bundle-7e0d4048d82daf111a35.js.map
+//# sourceMappingURL=bundle-cf68442f7b22adafb015.js.map
